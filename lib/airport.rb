8af23abebf7e5ae3e47b5fe188602cc61d2b352d
Sasha Cooper <jinksy@gmail.com>
2014-06-01 13:46:48 +0100

    Added partial airport functionality

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,19 @@
 class Airport
+
+	def planes
+		@planes ||= []
+	end
+
+	def plane_count
+		@planes.length
+	end
+
+	def bring_er_in(plane)
+		planes << plane.land
+	end
+
+	def boot_plane(plane)
+		planes.delete(plane.take_off)
+	end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,12 +8,34 @@ require 'airport'
 # If the airport is full then no planes can land
 describe Airport do
   let(:airport) { Airport.new }
+  let(:plane) { double :plane, land: :plane, take_off: :plane }
+
+  context 'plane storage' do
+  	it 'starts off with no planes' do
+  		expect(airport.planes).to eq []
+  	end
+  end
   
   context 'taking off and landing' do
     it 'a plane can land' do
+    	expect(plane).to receive(:land)
+    	airport.bring_er_in(plane)
+    end
+
+    it 'takes up one storage space when it lands' do
+    	airport.bring_er_in(plane)
+    	expect(airport.plane_count).to eq 1
     end
     
     it 'a plane can take off' do
+    	expect(plane).to receive(:take_off)
+    	airport.boot_plane(plane)
+    end
+
+    it 'stops being in the airport when it does' do
+    	airport.bring_er_in(plane)
+    	airport.boot_plane(plane)
+    	expect(airport.plane_count).to eq 0
     end
   end
   
